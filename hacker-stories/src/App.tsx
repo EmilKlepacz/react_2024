7db9e087aa7cf3d196f895bb5e78fc0721b5cdd2
Emil Klepacz <klepaczemil@gmail.com>
2024-06-21 13:34:42 +0200

    react props.

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -1,82 +1,90 @@
-import './App.css'
-import React from "react";
+import * as React from 'react';
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    }, {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    }
-];
+type Story = {
+    objectID: number;
+    url: string;
+    title: string;
+    author: string;
+    num_comments: number;
+    points: number;
+};
 
-const getTitle = (title: string) => title;
+const App = () => {
+    const stories = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
 
-const App = () =>
-    (
+    return (
         <div>
-            <h1>My Hacker News</h1>
+            <h1>My Hacker Stories</h1>
 
-            <h1>Hello {getTitle('React')}</h1>
+            <Search />
 
-            <Search/>
+            <hr />
 
-            <hr/>
-
-            <List/>
+            <List list={stories} />
         </div>
-    )
+    );
+};
 
 const Search = () => {
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        //sythetic event
+    const handleChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        // synthetic event
         console.log(event);
         // value of target (here: input HTML element)
         console.log(event.target.value);
     };
 
-    const handleOnBlur = (event: React.BaseSyntheticEvent) => {
-        //sythetic event
-        console.log("on blur triggered!");
-        console.log(event);
-        // value of target (here: input HTML element)
-        console.log(event.target.value);
-    }
-
-
     return (
         <div>
-            <label htmlFor="search">Search:</label>
-            <input id="search" type="text" onChange={handleChange} onBlur={handleOnBlur}/>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" onChange={handleChange} />
         </div>
     );
+};
 
-}
+type ListProps = {
+    list: Story[];
+};
 
-const List = () =>
-    (
-        <ul>
-            {list.map((item) => (
-                <li key={item.objectID}>
-                            <span>
-                                <a href={item.url}>{item.title}</a>
-                            </span>
-                    <span>{item.author}</span>
-                    <span>{item.num_comments}</span>
-                    <span>{item.points}</span>
-                </li>
-            ))}
-        </ul>
-    );
+const List = (props: ListProps) => (
+    <ul>
+        {props.list.map((item) => (
+            <Item key={item.objectID} item={item} />
+        ))}
+    </ul>
+);
 
+type ItemProps = {
+    item: Story;
+};
 
-export default App
+const Item = (props: ItemProps) => (
+    <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+        <span>{props.item.author}</span>
+        <span>{props.item.num_comments}</span>
+        <span>{props.item.points}</span>
+    </li>
+);
+
+export default App;
