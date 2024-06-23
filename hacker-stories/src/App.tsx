d3361adb5d15582f06f55ab0feb66b2132b96a8d
Emil Klepacz <klepaczemil@gmail.com>
2024-06-23 16:25:07 +0200

    controlled components introduced.

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -10,7 +10,7 @@ type Story = {
 };
 
 const App = () => {
-    const [searchTerm, setSearchTerm] = React.useState('');
+    const [searchTerm, setSearchTerm] = React.useState('React');
 
     const handleSearch = (
         event: React.ChangeEvent<HTMLInputElement>
@@ -45,7 +45,7 @@ const App = () => {
         <div>
             <h1>My Hacker Stories</h1>
 
-            <Search onSearch={handleSearch}/>
+            <Search onSearch={handleSearch} search={searchTerm}/>
 
             <hr/>
 
@@ -56,13 +56,18 @@ const App = () => {
 
 type SearchProps = {
     onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    search: string
 };
 
 const Search = (props: SearchProps) => {
     return (
         <div>
             <label htmlFor="search">Search: </label>
-            <input id="search" type="text" onChange={props.onSearch}/>
+            <input id="search"
+                   type="text"
+                   value={props.search}
+                   onChange={props.onSearch}/>
+
         </div>
     );
 };
