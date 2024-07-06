859c4cdcb0f31681766bef5ddddc19d215c6a694
Emil Klepacz <klepaczemil@gmail.com>
2024-07-06 17:53:27 +0200

    imperative react programming elements introduced.

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -154,21 +154,35 @@ type InputWithLabelProps = {
     type?: string,
     // label: string,
     value: string,
+    isFocused: boolean
     onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void
     children: React.ReactNode
 };
 
-const InputWithLabel = ({id, type = 'text', value, onInputChange, children}: InputWithLabelProps) => (
-    <>
-        <label htmlFor={id}>{children}</label> &nbsp;
-        <input
-            id={id}
-            type={type}
-            value={value}
-            onChange={onInputChange}
-        />
-    </>
-);
+const InputWithLabel = ({id, type = 'text', value, isFocused, onInputChange, children}: InputWithLabelProps) => {
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    React.useEffect(() => {
+        if (isFocused && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isFocused]);
+
+    return (
+        <>
+            <label htmlFor={id}>{children}</label> &nbsp;
+            <input
+                ref={inputRef}
+                id={id}
+                type={type}
+                value={value}
+                autoFocus={isFocused}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
+
 
 type ButtonProps = {
     text: string,
@@ -309,6 +323,7 @@ const App = () => {
             <InputWithLabel id="search"
                 // label="Search"
                             value={searchTerm}
+                            isFocused={true}
                             onInputChange={handleSearch}
             >
                 <strong>Search:</strong>
