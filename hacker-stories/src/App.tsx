cb94afe2d61df3cb16b005030aafd2e8144598c6
Emil Klepacz <klepaczemil@gmail.com>
2024-07-12 19:51:21 +0200

    slider implemented.

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -36,6 +36,80 @@ const StyledButton = styled.button`
     }
 `;
 
+const StyledSlider = styled.div`
+    position: relative;
+    border-radius: 3px;
+    background: #dddddd;
+    height: 15px;
+`;
+
+const StyledThumb = styled.div`
+    width: 10px;
+    height: 25px;
+    border-radius: 3px;
+    position: relative;
+    top: -5px;
+    opacity: 0.5;
+    background: #823eb7;
+    cursor: pointer;
+`;
+
+
+const getPercentage = (current: number, max: number) => (100 * current) / max;
+
+const getLeft = (percentage: number) => `calc(${percentage}% - 5px)`;
+
+const Slider = () => {
+    const sliderRef = React.useRef<HTMLDivElement>(null);
+    const thumbRef = React.useRef<HTMLDivElement>(null);
+
+    const diff = React.useRef<HTMLDivElement>(null);
+
+    const handleMouseMove = (event: MouseEvent) => {
+        let newX =
+            event.clientX -
+            diff.current -
+            sliderRef.current.getBoundingClientRect().left;
+
+        const end = sliderRef.current.offsetWidth - thumbRef.current.offsetWidth;
+
+        const start = 0;
+
+        if (newX < start) {
+            newX = 0;
+        }
+
+        if (newX > end) {
+            newX = end;
+        }
+
+        const newPercentage = getPercentage(newX, end);
+
+        thumbRef.current.style.left = getLeft(newPercentage);
+    };
+
+    const handleMouseUp = () => {
+        document.removeEventListener('mouseup', handleMouseUp);
+        document.removeEventListener('mousemove', handleMouseMove);
+    };
+
+    const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+        diff.current =
+            event.clientX - thumbRef.current.getBoundingClientRect().left;
+
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+    };
+
+    return (
+        <>
+            <StyledSlider ref={sliderRef}>
+                <StyledThumb ref={thumbRef} onMouseDown={handleMouseDown}/>
+            </StyledSlider>
+        </>
+    );
+};
+
 type DndItemProps = {
     index: number,
     item: User,
@@ -330,6 +404,12 @@ const App = () => {
     return (
         <div>
             <h1>My Hacker Stories</h1>
+
+            <h4>Slider component using refs example</h4>
+            <div>
+                <Slider/>
+            </div>
+            <br/><br/>
 
             <InputWithLabel id="search"
                 // label="Search"
