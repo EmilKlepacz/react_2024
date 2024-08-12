1abf323b00d395681952594c15018a7966a8439a
Emil Klepacz <klepaczemil@gmail.com>
2024-08-12 17:16:53 +0200

    native fetch API request replaced with axios lib.

--- hacker-stories/src/App.tsx
+++ hacker-stories/src/App.tsx
@@ -3,7 +3,7 @@ import {DragDropContext, Droppable, Draggable, DropResult} from 'react-beautiful
 import html2canvas from "html2canvas";
 import styled from 'styled-components';
 import {useState} from "react";
-
+import axios from "axios";
 
 type Story = {
     objectID: number;
@@ -416,12 +416,12 @@ const App = () => {
     const handleFetchStories = React.useCallback(() => {
         dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-        fetch(url)
-            .then((response) => response.json())
+        axios
+            .get(url)
             .then((result) => {
                 dispatchStories({
                     type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.hits,
+                    payload: result.data.hits,
                 });
             })
             .catch(() =>
